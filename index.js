ed63371e6c071aa3dee6d488bf5e863177f8b584
Joshua Ackerman <joshackerman757@gmail.com>
2023-12-06 17:02:36 -0500

    mod3

--- index.js
+++ index.js
@@ -2,36 +2,44 @@
  * [Exercise 1] trimProperties copies an object trimming its properties
  * @param {object} obj - an object with properties that are strings
  * @returns {object} - a copy of the object with strings trimmed
- *
- * EXAMPLE
- * trimProperties({ name: '  jane  ' }) // returns a new object { name: 'jane' }
  */
 function trimProperties(obj) {
-  // ✨ implement
+  const trimmedObj = {};
+  for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      trimmedObj[key] = obj[key].trim();
+    }
+  }
+  return trimmedObj;
 }
 
 /**
  * [Exercise 2] trimPropertiesMutation trims in place the properties of an object
  * @param {object} obj - an object with properties that are strings
  * @returns {object} - the same object with strings trimmed
- *
- * EXAMPLE
- * trimPropertiesMutation({ name: '  jane  ' }) // returns the object mutated in place { name: 'jane' }
  */
 function trimPropertiesMutation(obj) {
-  // ✨ implement
+  for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      obj[key] = obj[key].trim();
+    }
+  }
+  return obj;
 }
 
 /**
  * [Exercise 3] findLargestInteger finds the largest integer in an array of objects { integer: 1 }
  * @param {object[]} integers - an array of objects
  * @returns {number} - the largest integer
- *
- * EXAMPLE
- * findLargestInteger([{ integer: 1 }, { integer: 3 }, { integer: 2 }]) // returns 3
  */
 function findLargestInteger(integers) {
-  // ✨ implement
+  let maxInteger = -Infinity;
+  for (const obj of integers) {
+    if (obj && Object.prototype.hasOwnProperty.call(obj, 'integer') && typeof obj.integer === 'number') {
+      maxInteger = Math.max(maxInteger, obj.integer);
+    }
+  }
+  return maxInteger;
 }
 
 class Counter {
@@ -40,23 +48,17 @@ class Counter {
    * @param {number} initialNumber - the initial state of the count
    */
   constructor(initialNumber) {
-    // ✨ initialize whatever properties are needed
+    this.count = initialNumber;
   }
 
   /**
    * [Exercise 4B] Counter.prototype.countDown counts down to zero
    * @returns {number} - the next count, does not go below zero
-   *
-   * EXAMPLE
-   * const counter = new Counter(3)
-   * counter.countDown() // returns 3
-   * counter.countDown() // returns 2
-   * counter.countDown() // returns 1
-   * counter.countDown() // returns 0
-   * counter.countDown() // returns 0
    */
   countDown() {
-    // ✨ implement
+    const currentCount = this.count;
+    this.count = Math.max(0, this.count - 1);
+    return currentCount;
   }
 }
 
@@ -65,23 +67,18 @@ class Seasons {
    * [Exercise 5A] Seasons creates a seasons object
    */
   constructor() {
-    // ✨ initialize whatever properties are needed
+    this.seasons = ['summer', 'fall', 'winter', 'spring'];
+    this.currentSeasonIndex = 0;
   }
 
   /**
    * [Exercise 5B] Seasons.prototype.next returns the next season
    * @returns {string} - the next season starting with "summer"
-   *
-   * EXAMPLE
-   * const seasons = new Seasons()
-   * seasons.next() // returns "summer"
-   * seasons.next() // returns "fall"
-   * seasons.next() // returns "winter"
-   * seasons.next() // returns "spring"
-   * seasons.next() // returns "summer"
    */
   next() {
-    // ✨ implement
+    const nextSeason = this.seasons[this.currentSeasonIndex];
+    this.currentSeasonIndex = (this.currentSeasonIndex + 1) % this.seasons.length;
+    return nextSeason;
   }
 }
 
@@ -93,59 +90,49 @@ class Car {
    * @param {number} mpg - miles the car can drive per gallon of gas
    */
   constructor(name, tankSize, mpg) {
-    this.odometer = 0 // car initilizes with zero miles
-    this.tank = tankSize // car initiazes full of gas
-    // ✨ initialize whatever other properties are needed
+    this.name = name;
+    this.odometer = 0;
+    this.tank = tankSize;
+    this.mpg = mpg;
   }
 
   /**
    * [Exercise 6B] Car.prototype.drive adds miles to the odometer and consumes fuel according to mpg
    * @param {string} distance - the distance we want the car to drive
    * @returns {number} - the updated odometer value
-   *
-   * EXAMPLE
-   * const focus = new Car('focus', 20, 30)
-   * focus.drive(100) // returns 100
-   * focus.drive(100) // returns 200
-   * focus.drive(100) // returns 300
-   * focus.drive(200) // returns 500
-   * focus.drive(200) // returns 600 (ran out of gas after 100 miles)
    */
   drive(distance) {
-    // ✨ implement
+    const maxDistance = this.tank * this.mpg;
+    const actualDistance = Math.min(distance, maxDistance);
+    this.odometer += actualDistance;
+    this.tank -= actualDistance / this.mpg;
+    return this.odometer;
   }
 
   /**
    * [Exercise 6C] Adds gallons to the tank
    * @param {number} gallons - the gallons of fuel we want to put in the tank
    * @returns {number} - the miles that can be driven after refueling
-   *
-   * EXAMPLE
-   * const focus = new Car('focus', 20, 30)
-   * focus.drive(600) // returns 600
-   * focus.drive(1) // returns 600 (no distance driven as tank is empty)
-   * focus.refuel(99) // returns 600 (tank only holds 20)
    */
   refuel(gallons) {
-    // ✨ implement
+    const remainingSpace = 20 - this.tank; // Assuming the tank capacity is 20 gallons
+    const actualGallons = Math.min(gallons, remainingSpace);
+    this.tank += actualGallons;
+    return this.tank * this.mpg;
   }
 }
 
 /**
  * [Exercise 7] Asynchronously resolves whether a number is even
  * @param {number} number - the number to test for evenness
- * @returns {promise} - resolves true if number even, false otherwise
- *
- * EXAMPLE
- * isEvenNumberAsync(2).then(result => {
- *    // result is true
- * })
- * isEvenNumberAsync(3).then(result => {
- *    // result is false
- * })
+ * @returns {Promise} - resolves true if number even, false otherwise
  */
 function isEvenNumberAsync(number) {
-  // ✨ implement
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(number % 2 === 0);
+    }, 0);
+  });
 }
 
 module.exports = {
@@ -156,4 +143,4 @@ module.exports = {
   Counter,
   Seasons,
   Car,
-}
+};
